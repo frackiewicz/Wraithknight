8b3ed18041d0d260ad697b062bff03ec0b3b2418
Fillip Frackiewicz <fillip.frackiewicz@gmail.com>
2018-11-16 06:33:22 -0800

    little progress on levelgen

---
+++ Wraithknight/Classes/Levelgeneration/Level.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wraithknight
+{
+    struct Level
+    {
+        public bool[][] Walls;
+
+        public Level(int x, int y)
+        {
+            Walls = new bool[x][];
+            for (int i = 0; i < Walls.Length; i++)
+            {
+                Walls[i] = new bool[y];
+            }
+        }
+    }
+}
+    
---
+++ Wraithknight/Classes/Levelgeneration/LevelGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wraithknight
+{
+    class LevelGenerator
+    {
+        public Level GenerateLevel(Level level) //For now Cellular Automata
+        {
+            FillLevelWithRandomNoise(level, 50);
+
+            return level;
+        }
+
+        private void FillLevelWithRandomNoise(Level level, int percentWalls)
+        {
+            Random random = new Random();
+            for (int x = 0; x < level.Walls.Length; x++)
+            {
+                for (int y = 0; y < level.Walls[x].Length; y++)
+                {
+                    if (random.Next(0, 100) < percentWalls)
+                    {
+                        level.Walls[x][y] = true;
+                    }
+                }
+            }
+        }
+
+        private void ApplySimpleCellularAutomata(Level level)
+        {
+
+        }
+    }
+}
