3c15d8c3a7d1f3ee1d539a0b0a165df69de50ae6
Fillip Frackiewicz <fillip.frackiewicz@gmail.com>
2018-11-30 05:54:45 -0800

    starting with AI

---
+++ Wraithknight/Classes/ECS/Components/IntelligenceComponent.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wraithknight
+{
+    public enum OrderType
+    {
+        Attack1,
+        Attack2,
+        Follow,
+        Move
+    }
+    internal struct IntelligenceOrder
+    {
+        public EntityType Target;
+        public int Range;
+        public OrderType Order;
+        public int Priority;
+
+        IntelligenceOrder(EntityType target, int range, OrderType order, int priority)
+        {
+            Target = target;
+            Range = range;
+            Order = order;
+            Priority = priority;
+        }
+    }
+    class IntelligenceComponent : Component
+    {
+        public List<IntelligenceOrder> Orders = new List<IntelligenceOrder>();
+
+        public IntelligenceComponent(List<IntelligenceOrder> list)
+        {
+            if (Orders == null) Orders = list;
+            else Orders.AddRange(list);
+
+        }
+
+    }
+}
---
+++ Wraithknight/Classes/ECS/Components/IntelligenceNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Wraithknight
+{
+    class IntelligenceNode : Component
+    {
+        public EntityType Type;
+        public Vector2 Pos;
+
+        IntelligenceNode(EntityType type, Vector2 pos)
+        {
+            Type = type;
+            Pos = pos;
+        }
+    }
+}
